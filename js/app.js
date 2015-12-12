e52531ef85ea16b310712e92bf89dd658d82ccdf
Ben Thomas <bthomas2622@gmail.com>
2015-12-12 15:51:05 -0500

    markers are updating dynamically with search field

--- js/app.js
+++ js/app.js
@@ -41,8 +41,11 @@ var foodplaces = [
   },
 ];
 
+var map;
+var markers = [];
+
 function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 	  //center: {lat: 33.755, lng: -84.390},
 	  center: {lat: 33.800, lng: -84.390},
 	  zoom: 12,
@@ -52,39 +55,53 @@ function initMap() {
 	setMarkers(map, foodplaces);
 }
 
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
 function setMarkers(map, foodplaces) {
 	// Adds markers to the map.
 	// Marker sizes are expressed as a Size of X,Y where the origin of the image
 	// (0,0) is located in the top left of the image.
 	// Origins, anchor positions and coordinates of the marker increase in the X
 	// direction to the right and in the Y direction down.
-  var image = {
-    url: 'img/face.png',
-    // This marker is 20 pixels wide by 32 pixels high.
-    size: new google.maps.Size(25, 35),
-    // The origin for this image is (0, 0).
-    origin: new google.maps.Point(0, 0),
-    // The anchor for this image is the base of the flagpole at (0, 32).
-    anchor: new google.maps.Point(0, 35)
-  };
-  // Shapes define the clickable region of the icon. The type defines an HTML
-  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-  // The final coordinate closes the poly by connecting to the first coordinate.
-  var shape = {
-    coords: [1, 1, 1, 35, 25, 35, 25, 1],
-    type: 'poly'
-  };
-  for (var i = 0; i < foodplaces.length; i++) {
-    var foodplace = foodplaces[i];
-    var marker = new google.maps.Marker({
-      position: {lat: foodplace.lat, lng: foodplace.lon},
-      map: map,
-      icon: image,
-      shape: shape,
-      title: foodplace.name,
-      zIndex: foodplace.order
-    });
-  }
+	var image = {
+	url: 'img/face.png',
+	// This marker is 20 pixels wide by 32 pixels high.
+	size: new google.maps.Size(25, 35),
+	// The origin for this image is (0, 0).
+	origin: new google.maps.Point(0, 0),
+	// The anchor for this image is the base of the flagpole at (0, 32).
+	anchor: new google.maps.Point(0, 35)
+	};
+	// Shapes define the clickable region of the icon. The type defines an HTML
+	// <area> element 'poly' which traces out a polygon as a series of X,Y points.
+	// The final coordinate closes the poly by connecting to the first coordinate.
+	var shape = {
+	coords: [1, 1, 1, 35, 25, 35, 25, 1],
+	type: 'poly'
+	};
+
+	setMapOnAll(null);
+	markers = [];
+
+	for (var i = 0; i < foodplaces.length; i++) {
+		var foodplace = foodplaces[i];
+		if (foodplace.status) {
+		    var marker = new google.maps.Marker({
+		      position: {lat: foodplace.lat, lng: foodplace.lon},
+		      map: map,
+		      icon: image,
+		      shape: shape,
+		      title: foodplace.name,
+		      zIndex: foodplace.order
+		    });
+		    markers.push(marker);
+		}
+	}
 };
 
 var foodplaceModel = function(data){
@@ -115,13 +132,19 @@ var ViewModel = function () {
 	self.search = function(value){
 		self.foodList.removeAll();
 		for (var place in foodplaces){
+			foodplaces[place].status = false;
+		}
+		for (var place in foodplaces){
 			if (foodplaces[place].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
 				self.foodList.push(foodplaces[place]);
+				foodplaces[place].status = true;
 			}
 			else if (foodplaces[place].cuisine.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
 				self.foodList.push(foodplaces[place]);
+				foodplaces[place].status = true;
 			}
 		}
+		setMarkers(map, foodplaces);
 	}
 
 	self.input.subscribe(this.search);
